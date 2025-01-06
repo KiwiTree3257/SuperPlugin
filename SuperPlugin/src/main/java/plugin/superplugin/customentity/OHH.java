0585ef359dfccf39ec2369456d967c8ad15ea5d8
KiwiTree3257 <kiwitree3257@gmail.com>
2025-01-06 15:35:14 +0900

    eadawdawdadadada

--- SuperPlugin/src/main/java/plugin/superplugin/customentity/OHH.java
+++ SuperPlugin/src/main/java/plugin/superplugin/customentity/OHH.java
@@ -7,6 +7,7 @@ import org.bukkit.World;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.potion.PotionEffect;
@@ -15,12 +16,13 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.EulerAngle;
 import org.bukkit.util.Vector;
 import plugin.superplugin.CustomKeys;
+import plugin.superplugin.Function;
 import plugin.superplugin.SuperPlugin;
 
 import java.util.Random;
 
 public class OHH {
-    ArmorStand ohhEntity;
+    Zombie ohhEntity;
     Location ohhLocation;
     Location targetLocation;
     Vector moveDir;
@@ -33,22 +35,19 @@ public class OHH {
         world = player.getWorld();
         Location playerHeadLoc = player.getLocation().add(0, 1.5, 0);
         targetLocation = playerHeadLoc.clone().add(playerHeadLoc.getDirection().normalize());
+        Function.setLookAt(targetLocation, playerHeadLoc);
         moveDir = playerHeadLoc.getDirection().getCrossProduct(new Vector(0, 1, 0)).getCrossProduct(playerHeadLoc.getDirection()).normalize();
         ohhLocation = targetLocation.clone().add(moveDir.clone().multiply(-1.5));
-        ohhEntity = (ArmorStand) world.spawnEntity(ohhLocation.clone().add(0, -1.5, 0), EntityType.ARMOR_STAND);
-        ohhEntity.setHelmet(new ItemStack(Material.WITHER_SKELETON_SKULL));
+        ohhEntity = (Zombie) world.spawnEntity(ohhLocation.clone().add(0, -1.5, 0), EntityType.ZOMBIE);
+        ohhEntity.getEquipment().clear();
+        ohhEntity.getEquipment().setHelmet(new ItemStack(Material.WITHER_SKELETON_SKULL));
         ohhEntity.setInvisible(true);
         ohhEntity.setInvulnerable(true);
         ohhEntity.setSilent(true);
-        ohhEntity.setCanMove(false);
+        ohhEntity.setAI(false);
+        ohhEntity.setAdult();
         ohhEntity.setGravity(false);
-
-        double deltaX = playerHeadLoc.getX() - targetLocation.getX();
-        double deltaY = playerHeadLoc.getY() - targetLocation.getY();
-        double deltaZ = playerHeadLoc.getZ() - targetLocation.getZ();
-        double distance = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
-        double pitch = Math.atan2(deltaY, distance);
-        EulerAngle headPose = new EulerAngle(-pitch, ohhEntity.getHeadPose().getY() + Math.toRadians(180), 0); // Pitch는 음수로 설정해야 올바르게 보임
+        ohhEntity.setCollidable(false);
 
         Vector moveSpeed = moveDir.clone().multiply(speed);
 
@@ -60,7 +59,7 @@ public class OHH {
             boolean stop = false;
             @Override
             public void run() {
-                if (timer >= 2 * 20) {
+                if (timer >= lifeTIme) {
                     ohhEntity.remove();
                     player.getPersistentDataContainer().remove(CustomKeys.MOVE_STOP);
                     cancel();
@@ -76,7 +75,6 @@ public class OHH {
                 }
                 player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 40, 0, false, false));
                 ohhEntity.teleport(ohhLocation.clone().add(0, -1.5, 0));
-                ohhEntity.setHeadPose(headPose.add(Math.toRadians(random.nextInt(21)-10), Math.toRadians(random.nextInt(21)-10), 0));
                 timer++;
             }
         }.runTaskTimer(SuperPlugin.getInstance(), 0, 1);
